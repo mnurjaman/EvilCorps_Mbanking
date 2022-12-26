b9d59ee6dfbdab8d3eec47e64a7080d1d0238a32
Mnurjaman <mnurjaman035@gmail.com>
2022-12-26 11:44:26 +0700

    Initial Commit

---
+++ EvilCorps_Mbanking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EvilCorps_Mbanking
+//
+//  Created by Muhammad Nurjaman on 26/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
